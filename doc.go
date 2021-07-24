a1a90374a05f351fb994eaebef0fd47191658929
Dobrosław Żybort <matrixik@gmail.com>
2021-07-24 21:37:55 +0200

    Update doc.go

--- doc.go
+++ doc.go
@@ -4,19 +4,23 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 /*
-Package spinner implements text spinner.
+Package spinner implements text spinner. Useful for generating semirandom text.
 
-Features:
+## Features
 
- + Supported brackets: curly ({}) and square ([])
- + Supported text separators: | and ~
- + Unlimited nesting
++ Supported brackets: curly ({}) and square ([])
++ Supported text separators: | and ~
++ Unlimited nesting
 
-Example:
+## Example
 
 	text := "{Hello|Big} {world|people}!"
 	fmt.Println(spinner.Spin(text))
-	// Will print: Hello world! or Big world! or Hello people! or Big people!
+	// Posible outputs: 
+	//   Hello world!
+	//   Big world!
+	//   Hello people!
+	//   Big people!
 
 Requests or bugs?
 
